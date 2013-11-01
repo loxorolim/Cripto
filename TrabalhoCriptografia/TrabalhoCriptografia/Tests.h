df1daee2aaebfff9b39f9a01a27c190039a6c7c9
Mateus Azis <mateusazis@gmail.com>
2013-11-01 11:39:49 -0300

    Arquivos separados com arquivos header.

---
+++ TrabalhoCriptografia/TrabalhoCriptografia/Tests.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+void shiftRowsTest();
+void addRoundKeyTest();
+void testGenerateRoundKeys();
+void testMixColumns();
+void testeBoladoEBC();
+void testeBoladoCBC();
+
+#endif
---
+++ TrabalhoCriptografia/TrabalhoCriptografia/aes.h
@@ -0,0 +1,34 @@
+#ifndef AES_H
+#define AES_H
+
+#define EBC 1
+#define CBC 0
+
+typedef unsigned char byte;
+
+unsigned char gmul(unsigned char a, unsigned char b);
+void mixColumns(unsigned char* val, int index);
+
+void subBytes(byte *bytes, int count);
+
+void printMatrix(byte *matrix);
+
+void shiftRows(byte* matrix, int startingIndex);
+
+void addRoundKey(byte *state, int startingIndex, byte *subkey);
+
+void addRoundKeyTest();
+
+void makeRoundKey(int roundCount, byte **allKeys);
+void makeAllRoundKeys(int roundCount, byte **allKeys, byte *firstKey);
+void xor(byte * a, byte * b, byte * result);
+void doInitRound(byte * data, byte * result, byte* key, byte* toXor);
+void doRounds(byte * src, byte * dst, int rounds, byte ** allKeys);
+void doFinalRound(byte * src, byte * dst, int rounds, byte ** allKeys);
+void doAES(byte* data, byte** allKeys, int rounds, byte** toXor, byte* result, int type);
+void matrixTransposer(byte* data);
+void startAES(byte * data, int dataSize, byte * key, byte * result, int rounds, int type, byte * iv);
+long calculateOnBits(byte b);
+long calculateHammingDistance(byte * clearM, byte * criptoM, int arraySize);
+
+#endif
